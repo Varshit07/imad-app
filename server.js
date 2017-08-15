1e3acd6a89e38c512d9cabbbcb96f213cf02b0c4
Varshit Shah <varshitshah97@gmail.com>
2017-08-16 01:40:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-app.use(morgan('combined'));
+app.use(morgan('combined')); 
 
 var articles = {
-    var articleOne:{
-    title: 'Varshit Shah | Article One',
+    'article-one': {
+    title: 'Varshit\'s Article One',
     heading: 'Article One',
     date: 'Aug 15, 2017',
     content:` <p>
@@ -23,8 +23,8 @@ var articles = {
             <p>
                 This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. This is the content on article one. 
             </p>`
-},
-    var articleTwo: {
+    },
+    'article-two': {
         title: 'Varshit\'s Article Two',
         heading: 'Article Two',
         date: 'Aug 15, 2017',
@@ -35,7 +35,20 @@ var articles = {
                 This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. This is the content of article two. 
             </p>`
     },
-    var articleThree: {}
+    'article-three': {
+        title: 'Varshit\'s Article Three',
+        heading: 'Artcle Three',
+        date: 'Aug 15, 2017',
+        content: `<p>
+                This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. 
+                </p>
+                <p>
+                This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. 
+                </p>
+                <p>
+                This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. This is the content on Article 3. 
+                </p>`
+    }
 }
 function createTemplate(data){
     var title = data.title;
@@ -79,17 +92,11 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/article-one', function (req,res){
-   res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req,res){
+   res.send(createTemplate(articles[articleName]));
+   var articleName = req.params.articleName;
 });
 
-app.get('/article-two', function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function(req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
-});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));

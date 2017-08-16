deb3d24c39c6bb2bc49ccaf98b3780aab79b0c68
Varshit Shah <varshitshah97@gmail.com>
2017-08-16 20:50:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = "This is Varshit via JS!!";
 //Move Madi ;)
 var image = document.getElementById('madi');
-image.onclick = function(){
-  img.style.marginLeft = '50px';  
+image.onclick = function () {
+  image.style.marginLeft = '50px';  
 };

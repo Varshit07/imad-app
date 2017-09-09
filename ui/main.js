e1fb0ab632d6552956976e20e55c9da88c23ecfa
Varshit Shah <varshitshah97@gmail.com>
2017-09-09 19:34:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,7 @@
-console.log('Loaded!');
-//Change the text of main-text div element
-var element = document.getElementById('main-text');
-element.innerHTML = "This is Varshit via JS!!";
-//Move Madi ;)
-var image = document.getElementById('madi');
-marginLeft = 0;
-
-function moveRight () {
-  marginLeft = marginLeft + 3;
-  image.style.marginLeft = marginLeft + 'px';
-}
-
-image.onclick = function () {
-  var interval = setInterval(moveRight, 50); 
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function() {
+    counter++;
+    var span = document.getElementbyId('count');
+    span.innerHTML = counter.toString();
 };
